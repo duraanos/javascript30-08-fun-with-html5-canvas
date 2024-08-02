5f2685f163444a2fc921fdeaa62fd18174de1b01
duraanos <duran364@anadolu.edu.tr>
2024-08-02 14:38:29 +0300

    Add crazy colouring feature

--- script.js
+++ script.js
@@ -3,18 +3,43 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 80;
+
 let isDrawing = false;
+let hue = 0;
+let direction = true;
 
 const startDrawing = function (e) {
   isDrawing = true;
+
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
 };
 
 const draw = function (e) {
   if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+
+  if (ctx.lineWidth >= 80 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 };
 
 const stopDrawing = function () {
